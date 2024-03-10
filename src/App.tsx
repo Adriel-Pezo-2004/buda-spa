02b2e56f30c9557d19e7c88b64efa1c0707623a7
Adriel-Pezo-2004 <adriefape@gmail.com>
2024-03-09 19:29:14 -0500

    1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
 import "./index.css";
-import Inicio from "./inicio";
 
 function App() {
   return (
       <div className="App">
-        <Inicio />
         <Outlet />
       </div>
 
